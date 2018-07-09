01e5601fd49d8b8946f379769d291cf0a5d8eaa2
Jorge Castro <jorgecastro05@hotmail.com>
2018-07-09 14:54:11 -0500

    Added tests and converters

--- src/main/java/com/fuse/converter/ArrayConverter.java
+++ src/main/java/com/fuse/converter/ArrayConverter.java
@@ -2,23 +2,14 @@ package com.fuse.converter;
 
 import com.fuse.bean.MyArray;
 import org.apache.camel.Converter;
-import org.apache.camel.Exchange;
-import org.apache.camel.TypeConversionException;
-import org.apache.camel.support.TypeConverterSupport;
 
 import java.util.Vector;
 
 @Converter
-public class ArrayConverter extends TypeConverterSupport {
+public class ArrayConverter {
 
-    /*
-    public static Vector<String> toVector(ArrayList<String> aList) {
-         return new Vector<String>(aList);
-     */
-    @Override
-    public <T> T convertTo(Class<T> aClass, Exchange exchange, Object o) throws TypeConversionException {
-        MyArray obj = (MyArray) o;
-        Vector<String> result = new Vector<>(obj);
-        return (T) result;
+    @Converter
+    public static Vector<String> toVector(MyArray aList) {
+        return new Vector<String>(aList);
     }
 }
---
+++ src/test/java/com/fuse/RouteTest.java
@@ -0,0 +1,35 @@
+package com.fuse;
+
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class RouteTest extends CamelSpringTestSupport {
+
+    private String getMockEndpoint() {
+        return "mock:finish";
+    }
+
+    @Test
+    public void testRoute() throws Exception {
+        MockEndpoint mockEndpoint = getMockEndpoint(getMockEndpoint());
+        mockEndpoint.expectedMinimumMessageCount(1);
+        context.getRouteDefinition("routeConverter").adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() {
+                weaveAddLast().to(getMockEndpoint());
+            }
+        });
+
+        mockEndpoint.assertIsSatisfied();
+    }
+
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("spring/camel-context.xml");
+    }
+}
